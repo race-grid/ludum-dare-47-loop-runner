1b3a592c1ad61d4ff12d0ba24ec13e8f6dafda76
jmurray <jonte.murray@gmail.com>
2020-10-03 21:55:26 +0200

    Extract game state to object

--- game.js
+++ game.js
@@ -76,66 +76,47 @@ door_image.src = "assets/door.svg"
 var flag_image = new Image();
 flag_image.src = "assets/flag.svg"
 
-time_since_flip = 0;
-
 var w = 400;
 var h = 400;
 var cell_w = 40;
 
-const player_position = new_position(2, 0);
 
-var use_fire1 = true;
-var time_since_flip = 0;
-
-function update_character(position, dx, dy) {
+function update_character(game_state, dx, dy) {
+  position = game_state.player_position;
   const next_position = new_position(position.x + dx, position.y + dy);
-  if (contains_obstacle(next_position)) {
+  if (contains_obstacle(game_state, next_position)) {
     return;
   }
 
   position.x = next_position.x;
   position.y = next_position.y;
 
-  for (var i = 0; i < keys_and_doors.length; i++) {
-    if (character_occupies(position, keys_and_doors[i].key)) {
-      keys_and_doors.splice(i, 1);
+  for (var i = 0; i < game_state.keys_and_doors.length; i++) {
+    if (character_occupies(position, game_state.keys_and_doors[i].key)) {
+      game_state.keys_and_doors.splice(i, 1);
     }
   }
 
-  for (var i = 0; i < traps.length; i++) {
-    if (character_occupies(position, traps[i])) {
+  for (var i = 0; i < game_state.traps.length; i++) {
+    if (character_occupies(position, game_state.traps[i])) {
       console.log("YOU LOSE!");
-      traps.splice(i, 1);
-      game_over = true;
-      player_position.x = -1
-      player_position.y = -1
+      game_state.traps.splice(i, 1);
+      game_state.game_over = true;
+      game_state.player_position.x = -1
+      game_state.player_position.y = -1
     }
   }
-  if (character_occupies(position, goal_position)) {
+  if (character_occupies(position, game_state.goal_position)) {
     console.log("YOU WIN!");
-    game_over = true;
+    game_state.game_over = true;
   }
 }
 
-function contains_obstacle(position) {
-  for (var i = 0; i < obstacles.length; i++) {
-    if (character_occupies(position, obstacles[i])) {
-      return true;
-    }
-  }
-  for (var i = 0; i < keys_and_doors.length; i++) {
-    if (character_occupies(position, keys_and_doors[i].door)) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function update_objects(elapsed_time) {
-  time_since_flip += elapsed_time;
-  if (time_since_flip >= 500) {
+function update_objects(game_state, elapsed_time) {
+  game_state.time_since_flip += elapsed_time;
+  if (game_state.time_since_flip >= 500) {
     time_since_flip = 0;
-    use_fire1 = !use_fire1;
+    game_state.use_fire1 = !game_state.use_fire1;
   }
 }
 
@@ -143,44 +124,66 @@ function character_occupies(character, pos) {
   return character.x == pos.x && character.y == pos.y;
 }
 
-var traps = [new_position(0, 0), new_position(5, 5)];
-var obstacles = [
-  new_position(0, 1),
-  new_position(1, 1),
-  new_position(2, 1),
-  new_position(3, 1),
-  new_position(4, 1),
-  new_position(5, 1),
-];
-var keys_and_doors = [{ "key": new_position(1, 0), "door": new_position(4, 0) }]
-let goal_position = new_position(5, 0);
-
 function new_position(x, y) {
   return { "x": x, "y": y };
 }
 
-ctx.fillRect(player_position.x * cell_w, player_position.y * cell_w, cell_w, cell_w);
+game_state = {
+  time_since_flip: 0,
+  player_position: new_position(2, 0),
+  use_fire1: true,
+  traps: [new_position(0, 0), new_position(5, 5)],
+  obstacles: [
+               new_position(0, 1),
+               new_position(1, 1),
+               new_position(2, 1),
+               new_position(3, 1),
+               new_position(4, 1),
+               new_position(5, 1),
+             ],
+  keys_and_doors: [{ "key": new_position(1, 0), "door": new_position(4, 0) }],
+  goal_position: new_position(5, 0),
+  game_over: false,
+}
 
-function draw() {
+function contains_obstacle(game_state, position) {
+  for (var i = 0; i < game_state.obstacles.length; i++) {
+    if (character_occupies(position, game_state.obstacles[i])) {
+      return true;
+    }
+  }
+  for (var i = 0; i < game_state.keys_and_doors.length; i++) {
+    if (character_occupies(position, game_state.keys_and_doors[i].door)) {
+      return true;
+    }
+  }
+  return false;
+}
+
+function draw(game_state) {
   ctx.clearRect(0, 0, w, h);
 
-  ctx.drawImage(flag_image, (goal_position.x + .1) * cell_w, (goal_position.y + .1) * cell_w, cell_w * .8, cell_w * .9);
+  ctx.drawImage(
+    flag_image, (game_state.goal_position.x + .1) * cell_w,
+    (game_state.goal_position.y + .1) * cell_w, cell_w * .8, cell_w * .9);
 
   ctx.fillStyle = "#000000";
-  obstacles.forEach(o => {
+  game_state.obstacles.forEach(o => {
     ctx.fillRect(o.x * cell_w, o.y * cell_w, cell_w, cell_w, cell_w);
   });
 
-  keys_and_doors.forEach(o => {
+  game_state.keys_and_doors.forEach(o => {
     ctx.drawImage(key_image, o.key.x * cell_w, o.key.y * cell_w, cell_w, cell_w);
     ctx.drawImage(door_image, o.door.x * cell_w, o.door.y * cell_w, cell_w, cell_w);
   });
 
-  traps.forEach(o => {
-    ctx.drawImage(use_fire1 ? fire1_image : fire2_image, o.x * cell_w, o.y * cell_w, cell_w, cell_w);
+  game_state.traps.forEach(o => {
+    ctx.drawImage(game_state.use_fire1 ? fire1_image : fire2_image, o.x * cell_w, o.y * cell_w, cell_w, cell_w);
   });
 
-  ctx.drawImage(figure_image, player_position.x * cell_w, player_position.y * cell_w, cell_w, cell_w);
+  ctx.drawImage(
+    figure_image, game_state.player_position.x * cell_w,
+    game_state.player_position.y * cell_w, cell_w, cell_w);
 
   for (var x = 0; x < w; x += cell_w) {
     ctx.beginPath();
@@ -196,17 +199,15 @@ function draw() {
   }
 }
 
-var game_over = false;
-
 function loop(timestamp) {
   var elapsed_time = timestamp - lastRender;
 
-  if (!game_over) {
-    update_character(player_position, player_dx, player_dy);
+  if (!game_state.game_over) {
+    update_character(game_state, player_dx, player_dy);
     player_dx = player_dy = 0;
   }
   update_objects(elapsed_time);
-  draw();
+  draw(game_state);
 
   lastRender = timestamp;
   window.requestAnimationFrame(loop);
