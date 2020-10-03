334e1b2c023a688a0706ba85351c6da26a7f0d40
Samuel Murray <samuel.murray@outlook.com>
2020-10-03 16:58:37 +0200

    Replace box with stick figure

--- game.js
+++ game.js
@@ -3,6 +3,8 @@ console.log("Start of game.js");
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
 
+var figure = new Image();
+figure.src = "figure.svg";
 
 player_x = 0;
 var player_y = 0;
@@ -17,28 +19,28 @@ function update(elapsed_time) {
   if (time_since_movement >= 1000) {
     console.log("Moving player")
     time_since_movement = 0;
-    player_x ++;
+    player_x++;
   }
 }
 
 function draw() {
   ctx.clearRect(0, 0, w, h);
 
-  for (x = 0; x < w; x+= cell_w){
+  for (x = 0; x < w; x += cell_w) {
     ctx.beginPath();
     ctx.moveTo(x, 0);
     ctx.lineTo(x, h);
     ctx.stroke();
   }
-  for (y = 0; y < h; y+= cell_w){
+  for (y = 0; y < h; y += cell_w) {
     ctx.beginPath();
     ctx.moveTo(0, y);
     ctx.lineTo(w, y);
     ctx.stroke();
   }
-
-  ctx.fillRect(player_x * cell_w, player_y * cell_w, cell_w, cell_w);
+  ctx.drawImage(figure, player_x * cell_w, player_y * cell_w, cell_w, cell_w);
 }
+
 
 function loop(timestamp) {
   var elapsed_time = timestamp - lastRender;
